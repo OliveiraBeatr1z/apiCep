85e60696fc6e4dd59b675beca7c458faf5226b77
OliveiraBeatr1z <luamorningstar3@gmail.com>
2025-08-10 13:59:38 -0300

    api funcionando

--- aplicacao/src/main/java/org/aplicacao/Main.java
+++ aplicacao/src/main/java/org/aplicacao/Main.java
@@ -1,7 +1,25 @@
 package org.aplicacao;
 
+import java.io.IOException;
+
+import org.aplicacao.dto.EnderecoDto;
+import org.aplicacao.servico.ApiServico;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       ApiServico apiServico = new ApiServico();
+       try {
+        EnderecoDto endereco = apiServico.getEndereco("03805110");
+        System.out.println("Logradouro: " + endereco.getLogradouro());
+        System.out.println("Bairro: " + endereco.getBairro());
+        System.out.println("Cidade: " + endereco.getLocalidade());
+        System.out.println("UF: " + endereco.getUf());
+        System.out.println("CEP: " + endereco.getCep());
+       } catch (IOException e) {
+        throw new RuntimeException();
+       } catch (InterruptedException e) {
+        throw new RuntimeException("Erro inesperado: " + e.getMessage(), e);
+       }
+       System.out.println("Programa finalizado.");
     }
 }
--- aplicacao/src/main/java/org/aplicacao/servico/ApiServico.java
+++ aplicacao/src/main/java/org/aplicacao/servico/ApiServico.java
@@ -5,19 +5,26 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
 import org.aplicacao.dto.EnderecoDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ApiServico {
+
+    EnderecoDto enderecoDto = new EnderecoDto();
     
     public EnderecoDto getEndereco(String cep) throws IOException, InterruptedException {
         try {
-            HttpClient client = HttpClient.newHttpClient();
+            HttpClient client = HttpClient.newHttpClient(); // cria as variaveis para a requisição HTTP
             HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://viacep.com.br/ws/" + cep + "/json/")).build();
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()); // envia a requisição e recebe a resposta como String
+
+                ObjectMapper mapper = new ObjectMapper(); //deserializa o JSON recebido
+                enderecoDto = mapper.readValue(response.body(), EnderecoDto.class); // recebe o JSON e converte para EnderecoDto (cria o objeto java)
+
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
+        return enderecoDto; // retorna o objeto EnderecoDto
     }
 }
